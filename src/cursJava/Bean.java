87266f00101e28bc8829501d44ba12bd31be2724
Hp <Hp@DESKTOP-VQHC3OU>
2018-09-09 17:51:44 +0300

    unde pula meafac commit?

---
+++ src/cursJava/Bean.java
@@ -0,0 +1,21 @@
+package cursJava;
+
+public class Bean {
+	
+	private int var1;
+
+	public int getVar1() {
+		return var1;
+	}
+
+	public void setVar1(int var1) {
+		this.var1 = var1;
+	}
+
+	public Bean(int var1) {
+		super();
+		this.var1 = var1;
+	} 
+
+	
+}
