ddb9fd675271cf65cf456f115e18e36444a19deb
andLivProj <lupascu.liviu@yahoo.com>
2019-05-05 17:50:47 +0300

    test

---
+++ src/cursJava/HelloWorld2.java
@@ -0,0 +1,24 @@
+package cursJava;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hello = "Hello World!";
+		
+		System.out.println(hello);
+		System.out.println('b');
+		
+		int[] numbers = {1,2,3,4,5};
+		int sum = 0;
+		for (int number : numbers) {
+			
+			 sum += number;
+		}
+		
+
+		System.out.println(sum);
+	}
+
+}
